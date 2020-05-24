48eec72a0fe2ad6dc8185edf05acc0ee449bab69
JaredMathis <jaredmathis@protonmail.com>
2020-05-23 22:23:32 -0400

    update

--- web/app.js
+++ web/app.js
@@ -1,3 +1,27 @@
+const countries = [
+    "United States",
+    "Mexico"
+];
+
+const screens = {
+    selectedCountry: 'selectedCountry',
+};
+
+
 angular.module('app', []);
 
-angular.module('app');
+angular.module('app').controller('HomeController', 
+($scope)=>{
+    $scope.screens = screens;
+
+    $scope.state = {};
+
+    $scope.state.screen = screens.selectedCountry;
+
+    // TODO: auto-detect country
+    $scope.state.selectedCountry = 'United States';
+
+    $scope.countries = countries;
+
+    $scope.todo = () => alert('TODO');
+});

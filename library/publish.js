a0936cf9d0515d1d4a1d0b1913b3a6334aa4ea2c
JaredMathis <jaredmathis@protonmail.com>
2020-05-25 13:18:02 -0400

    updated npm package library

--- library/publish.js
+++ library/publish.js
@@ -2,11 +2,13 @@ const {
     execSync,
 } = require("child_process");
 
+const path = require('path');
+
 // Run tests before bumping.
 require('../test');
 
 const u = require('wlj-utilities');
 
-u.bumpPackageVersion();
+u.bumpPackageVersion(__dirname);
 
-execSync('npm publish without-ceasing-library');
+execSync('npm publish');

8ec89c4bcbb8e0bcb5fdb86bb5a57256e1cebc62
JaredMathis <jaredmathis@protonmail.com>
2020-05-26 13:41:03 -0400

    update

--- functions/index.js
+++ functions/index.js
@@ -5,9 +5,13 @@ admin.initializeApp();
 const { 
     ask,
     prayersAreEqual,
+    countries,
 } = require('without-ceasing-library');
 
+const u = require('wlj-utilities');
+
 const prayersRoute = 'prayers';
+const intercessionsRoute = 'intercessions';
 
 exports.health = functions.https.onRequest(async (req, res) => {
     allowCORS(res);
@@ -15,7 +19,7 @@ exports.health = functions.https.onRequest(async (req, res) => {
     res.json({success:true});
 });
 
-exports.ask = functions.https.onRequest(async (req, res) => {
+exports.pray = functions.https.onRequest(async (req, res) => {
     allowCORS(res);
 
     let userId = req.query.userId;
@@ -54,13 +58,18 @@ exports.ask = functions.https.onRequest(async (req, res) => {
     res.json({success:true});
 });
 
-exports.pray = functions.https.onRequest(async (req, res) => {
+exports.intercessions = functions.https.onRequest(async (req, res) => {
     allowCORS(res);
 
-    let userId = req.query.userId;
+    let prayerUserId;
+    u.scope('intercessions validation', x => {
+        u.merge(x, {query:req.query});
+    
+        prayerUserId = req.query.prayerUserId;
+        u.assert(() => u.isString(prayerUserId));
+    });
 
-    let prayerUserId = req.query.prayerUserId;
-    let result = ask({
+    let expected = ask({
         letter: req.query.letter,
         petition: req.query.petition,
         userId: prayerUserId,
@@ -69,30 +78,112 @@ exports.pray = functions.https.onRequest(async (req, res) => {
     let userPrayers = admin.database().ref(`/user/${prayerUserId}/${prayersRoute}`);
     let userPrayersValue = await userPrayers.once('value');
 
-    let allUserPrayers = JSON.parse(JSON.stringify(userPrayersValue));
+    let allUserPrayers = clone(userPrayersValue);
     if (!allUserPrayers) {
         res.json({success:false, message:'No prayers for user'});
+        return;
     }
+
+    let keys = Object.keys(allUserPrayers);
+    let promises = keys.map(key => {
+        let result = {};
+
+        let existing = allUserPrayers[key];
+        if (prayersAreEqual(expected, existing)) {
+            result.prayersAreEqual = true;
+            // Loop through intercessions for prayer
+            let intercessions = admin.database().ref(`/${intercessionsRoute}/${prayersRoute}/${key}`);
+            result.promise = intercessions.once('value')
+                .then(v => result.intercessionsOnce = v);
+        } else {
+            result.promise = new Promise();
+        }
+        return result; 
+    });
+
+    await Promise.all(promises.map(p => p.promise));
+
+    for (let p of promises) {
+        if (p.prayersAreEqual) {
+            let intercessionsValue = clone(p.intercessionsOnce);
+            console.log(intercessionsValue);
+            res.json(intercessionsValue);
+            return;
+        }
+    }
+
+    res.json({success:false, message:'did not find intercessions'});
+});
+
+exports.intercede = functions.https.onRequest(async (req, res) => {
+    allowCORS(res);
+
+    let prayerUserId;
+    let userId;
+    let country;
+    u.scope('intercede validation', x => {
+        u.merge(x, {query:req.query});
+
+        userId = req.query.userId;
+        u.assert(() => u.isString(userId));
+
+        country = req.query.country;
+        u.assert(() => u.isString(country));
+        u.assert(() => countries.includes(country));
+    
+        prayerUserId = req.query.prayerUserId;
+        u.assert(() => u.isString(prayerUserId));
+    });
+
+    let userPrayers = admin.database().ref(`/user/${prayerUserId}/${prayersRoute}`);
+    let userPrayersValue = await userPrayers.once('value');
+
+    let allUserPrayers = clone(userPrayersValue);
+    if (!allUserPrayers) {
+        res.json({success:false, message:'No prayers for user'});
+        return;
+    }
+
+    let expected = ask({
+        letter: req.query.letter,
+        petition: req.query.petition,
+        userId: prayerUserId,
+    });
+
     let keys = Object.keys(allUserPrayers);
     for (let key of keys) {
-        let prayer = allUserPrayers[key];
-        if (prayersAreEqual(result, prayer)) {
-            console.log({here: userPrayersValue[key]});
+        let existing = allUserPrayers[key];
+        if (prayersAreEqual(expected, existing)) {
+            // Loop through intercessions for prayer
+            let intercessions = admin.database().ref(`/${intercessionsRoute}/${prayersRoute}/${key}`);
+            let intercessionsOnce = await intercessions.once('value');
+            let intercessionsValue = clone(intercessionsOnce);
+            console.log({intercessionsValue});
+            if (intercessionsValue) {
+                for (let keyI in intercessionsValue) {
+                    let i = intercessionsValue[keyI];
+                    if (i.userId === userId) {
+                        res.json({
+                            success:true,
+                            message:'already interceded',
+                        });
+                        return;
+                    }
+                }
+            }
+            await intercessions.push({
+                userId,
+                country,
+            });
             res.json({
                 success:true,
+                message:'interceded',
             });
             return;
         }
     }
 
-    let userRequestsChild = await userPrayers.push();
-    await userRequestsChild.set(result);
-
-    let prayers = admin.database().ref(`/${prayersRoute}`);
-    let prayersChild = await prayers.push();
-    await prayersChild.set(result);
-
-    res.json({success:true});
+    res.json({success:false, message:'did not find prayer'});
 });
 
 exports.prayers = functions.https.onRequest(async (req, res) => {
@@ -106,4 +197,8 @@ exports.prayers = functions.https.onRequest(async (req, res) => {
 function allowCORS(res) {
   res.set('Access-Control-Allow-Origin', "*")
   res.set('Access-Control-Allow-Methods', 'GET, POST')
+}
+
+function clone(item) {
+    return JSON.parse(JSON.stringify(item))
 }
--- tests/functions/test.js
+++ tests/functions/test.js
@@ -7,9 +7,11 @@ const local = "http://localhost:5001/without-ceasing/us-central1/";
 const production = 'https://us-central1-without-ceasing.cloudfunctions.net/';
 
 const server = local;
-const health = server + "health";
-const requests = server + "prayers";
-const ask = server + "ask";
+const healthRoute = server + "health";
+const prayersRoute = server + "prayers";
+const prayRoute = server + "pray";
+const intercedeRoute = server + "intercede";
+const intercessionsRoute = server + "intercessions";
 
 u.scope(__filename, x => {
     if (!isServerRunning()) {
@@ -17,11 +19,12 @@ u.scope(__filename, x => {
     }
 
     ask2PrayersShouldShowUp();
+    intercedeForPrayer();
 });
 
 function askPrayer(letter, petition, userId) {
     u.scope(askPrayer.name, x => {
-        let r = request('GET', ask + u.toQueryString({letter,petition,userId}));
+        let r = request('GET', prayRoute + u.toQueryString({letter,petition,userId}));
         let rBody = r.body.toString();
         u.merge(x,{rBody});
 
@@ -32,26 +35,81 @@ function askPrayer(letter, petition, userId) {
     });
 }
 
+function intercede(letter, petition, prayerUserId,userId,country) {
+    u.scope(intercede.name, x => {
+        let qso = {letter,petition,prayerUserId,userId,country};
+        let qs = u.toQueryString(qso);
+        u.merge(x, {qs});
+        let route = intercedeRoute + qs;
+        u.merge(x, {route});
+        let r = request('GET',route );
+        let rBody = r.body.toString();
+        u.merge(x,{rBody});
+
+        let body = JSON.parse(r.body);
+        u.merge(x,{body});
+        u.assert(() => u.isDefined(body.success));
+        u.assert(() => body.success === true);
+        u.assert(() => ['interceded', 'already interceded'].includes(body.message));
+    });
+}
+
 function ask2PrayersShouldShowUp() {
     let result;
     u.scope(ask2PrayersShouldShowUp.name, x => {
         askPrayer('J', 'Wisdom', '1234');
         askPrayer('J', 'Wisdom', '1235');
 
-        u.merge(x, {step:'making request'});
-        let r = request('GET', requests);
+        assertTotalPrayersAtLeast(2);
+    });
+    return result;
+}
 
+function assertTotalPrayersAtLeast(prayerCount) {
+    u.scope(assertTotalPrayersAtLeast.name, x => {
+        u.merge(x, {step:'making request'});
+        let r = request('GET', prayersRoute);
+    
         u.merge(x, {step:'parsing'});
         let body = JSON.parse(r.body.toString());
-
+    
         u.merge(x, {step:'getting keys'});
         u.merge(x, {body});
         let keys = Object.keys(body);
-
+    
         u.merge(x, {keys});
-        u.assert(() => keys.length === 2);
+        u.assert(() => keys.length === prayerCount);
+    })
+}
+
+function intercedeForPrayer() {
+    let result;
+    u.scope(intercedeForPrayer.name, x => {
+        intercede('J', 'Wisdom', '1234', '1236', 'United States');
+        intercede('J', 'Wisdom', '1234', '1237', 'Mexico');
+        intercede('J', 'Wisdom', '1234', '1238', 'Mexico');
+
+        assertTotalIntercessionsAtLeast('J', 'Wisdom', '1234', 3);
     });
     return result;
+
+}
+
+function assertTotalIntercessionsAtLeast(letter, petition, userId, intercessionCount) {
+    u.scope(assertTotalIntercessionsAtLeast.name, x => {
+        u.merge(x, {step:'making request'});
+        let r = request('GET', intercessionsRoute + u.toQueryString({letter,petition,prayerUserId:userId}));
+    
+        u.merge(x, {step:'parsing'});
+        let body = JSON.parse(r.body.toString());
+    
+        u.merge(x, {step:'getting keys'});
+        u.merge(x, {body});
+        let keys = Object.keys(body);
+    
+        u.merge(x, {keys});
+        u.assert(() => keys.length === intercessionCount);
+    })
 }
 
 function isServerRunning() {
@@ -59,7 +117,7 @@ function isServerRunning() {
     u.scope(isServerRunning.name, x => {
         let h;
         try {
-            h = request('GET', health);
+            h = request('GET', healthRoute);
         } catch (e) {
             if (e.message === "connect ECONNREFUSED 127.0.0.1:5001") {
                 console.log('Emulator is not running.');
